ebf9494d4edf6620e7c2e761ba1826833e90f2b1
smazoomder@gmail.com <smazoomder@gmail.com>
2019-12-01 20:30:02 +0600

    0.0.0.2.1

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'autobot',
   packages = ['autobot'],
-  version = '0.0.0.1.1',
+  version = '0.0.0.2.1',
   long_description=long_description,
   long_description_content_type='text/markdown',
   author = 'Shaon Majumder',
   author_email = 'smazoomder@gmail.com',
   url = 'https://github.com/ShaonMajumder/autobot',
-  download_url = 'https://github.com/ShaonMajumder/autobot/archive/0.0.0.1.1.tar.gz',
+  download_url = 'https://github.com/ShaonMajumder/autobot/archive/0.0.0.2.1.tar.gz',
   keywords = ['shaon', 'utility', 'statistics'], 
   classifiers = [],
 )
