b2a803c3fa6ceaf5517c91aeb7300d1b1a1f550a
smazoomder@gmail.com <smazoomder@gmail.com>
2019-12-01 17:34:18 +0600

    work started

---
+++ setup.py
@@ -0,0 +1,22 @@
+#from distutils.core import setup
+from setuptools import setup
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'autobot',
+  packages = ['autobot'],
+  version = '0.0.0.1.1',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Shaon Majumder',
+  author_email = 'smazoomder@gmail.com',
+  url = 'https://github.com/ShaonMajumder/autobot',
+  download_url = 'https://github.com/ShaonMajumder/autobot/archive/0.0.0.1.1.tar.gz',
+  keywords = ['shaon', 'utility', 'statistics'], 
+  classifiers = [],
+)
